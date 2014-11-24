9cbb49aa95cbc29e166d44f1940da8dfe7733771
izzy84075 <izzy84075@gmail.com>
2014-11-24 13:37:07 -0800

    Conversion between the simplified and verbose LTTO_IR signature formats.

--- src/LTTO_IR.hpp
+++ src/LTTO_IR.hpp
@@ -7,7 +7,8 @@ typedef enum {
 	eLTTO_IR_SIGNATURETYPE_TAG,
 	eLTTO_IR_SIGNATURETYPE_MULTIBYTE_PTYPE,
 	eLTTO_IR_SIGNATURETYPE_MULTIBYTE_DATA,
-	eLTTO_IR_SIGNATURETYPE_MULTIBYTE_CSUM
+	eLTTO_IR_SIGNATURETYPE_MULTIBYTE_CSUM,
+	eLTTO_IR_SIGNATURETYPE_INVALID
 } eLTTO_IR_SIGNATURETYPE;
 
 typedef enum {
@@ -40,11 +41,102 @@ typedef struct {
 		SFP = eLTTO_IR_SFP_SHORT;
 		data = 0;
 	}
+	
+	bool convert(LTTO_IR_SIGNATURE *output) {
+		switch(headerType) {
+			case eLTTO_IR_HEADERTYPE_NORMAL:
+				//Tag, multibyte PType, multibyte data, or multibyte CSum
+				switch(bitCount) {
+					case eLTTO_IR_BITCOUNT_7:
+						//Tag
+						output->sigType = eLTTO_IR_SIGNATURETYPE_TAG;
+						break;
+					case eLTTO_IR_BITCOUNT_8:
+						//Multibyte data
+						output->sigType = eLTTO_IR_SIGNATURETYPE_MULTIBYTE_DATA;
+						break;
+					case eLTTO_IR_BITCOUNT_9:
+						//Multibyte PType or multibyte CSum
+						if(data & 0x0100) {
+							//Multibyte CSum
+							output->sigType = eLTTO_IR_SIGNATURETYPE_MULTIBYTE_CSUM;
+						} else {
+							//Multibyte PType
+							eLTTO_IR_SIGNATURETYPE_MULTIBYTE_PTYPE;
+						}
+						break;
+					default:
+						//Invalid
+						output->sigType = eLTTO_IR_SIGNATURETYPE_INVALID;
+						return false;
+				}
+				break;
+			case eLTTO_IR_HEADERTYPE_BEACON:
+				//LTTO beacon or LTAR beacon
+				switch(bitCount) {
+					case eLTTO_IR_BITCOUNT_5:
+						//LTTO beacon
+						output->sigType = eLTTO_IR_SIGNATURETYPE_LTTO_BEACON;
+						break;
+					case eLTTO_IR_BITCOUNT_9:
+						//LTAR beacon
+						output->sigType = eLTTO_IR_SIGNATURETYPE_LTAR_BEACON;
+						break;
+					default:
+						output->sigType = eLTTO_IR_SIGNATURETYPE_INVALID;
+						return false;
+				}
+				break;
+		}
+		
+		output->data = (uint8_t)(data);
+		
+		return true;
+	}
 } LTTO_IR;
 
 typedef struct {
 	eLTTO_IR_SIGNATURETYPE sigType;
 	uint8_t data;
+	
+	void convert(LTTO_IR *output) {
+		output->data = 0;
+		switch(sigType) {
+			case eLTTO_IR_SIGNATURETYPE_LTTO_BEACON:
+				output->headerType = eLTTO_IR_HEADERTYPE_BEACON;
+				output->bitCount = eLTTO_IR_BITCOUNT_5;
+				output->SFP = eLTTO_IR_SFP_SHORT;
+				break;
+			case eLTTO_IR_SIGNATURETYPE_LTAR_BEACON:
+				output->headerType = eLTTO_IR_HEADERTYPE_BEACON;
+				output->bitCount = eLTTO_IR_BITCOUNT_9;
+				output->SFP = eLTTO_IR_SFP_SHORT;
+				break;
+			case eLTTO_IR_SIGNATURETYPE_TAG:
+				output->headerType = eLTTO_IR_HEADERTYPE_NORMAL;
+				output->bitCount = eLTTO_IR_BITCOUNT_7;
+				output->SFP = eLTTO_IR_SFP_MEDIUM;
+				break;
+			case eLTTO_IR_SIGNATURETYPE_MULTIBYTE_PTYPE:
+				output->headerType = eLTTO_IR_HEADERTYPE_NORMAL;
+				output->bitCount = eLTTO_IR_BITCOUNT_9;
+				output->SFP = eLTTO_IR_SFP_SHORT;
+				break;
+			case eLTTO_IR_SIGNATURETYPE_MULTIBYTE_DATA:
+				output->headerType = eLTTO_IR_HEADERTYPE_NORMAL;
+				output->bitCount = eLTTO_IR_BITCOUNT_8;
+				output->SFP = eLTTO_IR_SFP_SHORT;
+				break;
+			case eLTTO_IR_SIGNATURETYPE_MULTIBYTE_CSUM:
+				output->headerType = eLTTO_IR_HEADERTYPE_NORMAL;
+				output->bitCount = eLTTO_IR_BITCOUNT_9;
+				output->SFP = eLTTO_IR_SFP_LONG;
+				output->data |= 0x0100;
+				break;
+		}
+		
+		output->data |= data;
+	}
 } LTTO_IR_SIGNATURE;
 
 typedef struct {
