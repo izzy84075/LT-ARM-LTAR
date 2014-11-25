eab01bcb18f38021d3b68bf4a0328d81b65265bf
izzy84075 <izzy84075@gmail.com>
2014-11-24 23:10:09 -0800

    Fold LTTO_IRTX_WRAPPER into LTTO_IRTX, while refactoring LTTO_IRTX to be a bit more straightforward to use.

--- src/LTTO_IRTX.hpp
+++ src/LTTO_IRTX.hpp
@@ -6,18 +6,29 @@
 class LTTO_IRTX {
 	public:
 		LTTO_IRTX( void (*IRLEDON)(void), void (*IRLEDOFF)(void));
-		bool queueSignature(LTTO_IR signature);
-		bool signatureInProgress(void);
-		void LTTO_IRTX_1msTick(void);
 		void reset(void);
+		void LTTO_IRTX_1msTick(void);
+		
+		bool enqueue(eLTTO_IR_SIGNATURETYPE sigType, uint16_t data);
+		bool enqueue(LTTO_IR_SIGNATURE signature);
+		bool enqueue(LTTO_IR_MULTIBYTE structure);
+		bool enqueue(LTTO_IR signature);
+		
+		bool isBusy(void);
 	private:
 		LTTO_IR workingBuffer;
+		LTTO_IR_MULTIBYTE multibyteBuffer;
+		
 		unsigned int timer;
 		unsigned int step;
 		unsigned int dataCounter;
+		unsigned int multibytePosition;
 		bool busy;
+		
 		void (*LEDON)(void);
 		void (*LEDOFF)(void);
+		
+		bool tryAdvancingMultibyte(void);
 };
 
 #endif
